a4448e03e7ef1eba918a0b5c9b3940d4af1abc2f
onkar2405 <onkarvaidyad789@gmail.com>
2024-11-04 23:09:00 +0530

    Improve naive for distinct patterns

--- Strings/PatternSearch.cpp
+++ Strings/PatternSearch.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 
+/**
+ * This function works for all types of patterns i.e. distinct and non-distinct.
+ */
 void findPatternNaive(string str, string pattern)
 {
   for (int i = 0; i <= str.length() - pattern.length(); i++)
@@ -24,12 +27,44 @@ void findPatternNaive(string str, string pattern)
   }
 }
 
+/**
+ * This function works only for distinct types of patterns i.e. ABCD.
+ */
+void findPatternImprovedNaiveOnlyDistinct(string str, string pattern)
+{
+  int m = str.length();
+  int n = pattern.length();
+
+  for (int i = 0; i <= m - n; i++)
+  {
+    if (pattern[0] == str[i])
+    {
+      int j = 1;
+      while (j < n)
+      {
+        if (pattern[j] != str[i + j])
+        {
+          i = i + j - 1;
+          break;
+        }
+        j++;
+      }
+
+      if (j == n)
+      {
+        cout << i << " ";
+      }
+    }
+  }
+}
+
 int main()
 {
-  string str = "AAAA";
-  string pattern = "AA";
+  string str = "ABCABCDDABCD";
+  string pattern = "ABCD";
 
   findPatternNaive(str, pattern);
+  findPatternImprovedNaiveOnlyDistinct(str, pattern);
 
   return 0;
 }
