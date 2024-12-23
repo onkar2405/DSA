794b47b4ab7020aaf3b88ef6ff02bb7a22631549
onkar2405 <onkarvaidyad789@gmail.com>
2024-12-24 00:19:55 +0530

    Tree Easy V2:

---
+++ LeetCodeTop150/Binary Search Tree/CountNodes.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+/**
+ * https://leetcode.com/problems/path-sum/?envType=study-plan-v2&envId=top-interview-150
+ */
+
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+bool countNodes(TreeNode *root)
+{
+  if (root == NULL)
+  {
+    return 0;
+  }
+
+  return 1 + countNodes(root->left) + countNodes(root->right);
+}
