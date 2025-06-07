c95f9b9c11c8966ed9a4b5036823a49158dbcad1
onkar2405 <onkarvaidyad789@gmail.com>
2025-06-08 00:12:01 +0530

    Heap Sort

--- BinaryHeap/HeapSort.cpp
+++ BinaryHeap/HeapSort.cpp
@@ -2,7 +2,17 @@
 #include <vector>
 using namespace std;
 
-void heapify(vector<int> &heap, int parentIndex)
+void printHeap(vector<int> binaryHeap)
+{
+  for (int i = 0; i < binaryHeap.size(); i++)
+  {
+    cout << binaryHeap[i] << " ";
+  }
+
+  cout << "\n";
+}
+
+void maxHeapify(vector<int> &heap, int parentIndex)
 {
   int parentVal = heap[parentIndex];
   int leftIndex = 2 * parentIndex + 1;
@@ -17,7 +27,32 @@ void heapify(vector<int> &heap, int parentIndex)
     maxIndex = rightIndex;
   }
 
-  swap(heap[parentIndex], heap[maxIndex]);
+  if (maxIndex != parentIndex)
+  {
+    swap(heap[parentIndex], heap[maxIndex]);
+    maxHeapify(heap, maxIndex);
+  }
+}
+
+void heapify(vector<int> &heap, int size, int parentIndex)
+{
+  int parentVal = heap[parentIndex];
+  int leftIndex = 2 * parentIndex + 1;
+  int rightIndex = 2 * parentIndex + 2;
+  int maxIndex = parentIndex;
+  if (leftIndex < size && parentVal < heap[leftIndex])
+  {
+    maxIndex = leftIndex;
+  }
+  if (rightIndex < size && heap[maxIndex] < heap[rightIndex])
+  {
+    maxIndex = rightIndex;
+  }
+
+  if (maxIndex != parentIndex)
+  {
+    swap(heap[parentIndex], heap[maxIndex]);
+  }
 }
 
 void maxHeap(vector<int> &heap)
@@ -26,29 +61,31 @@ void maxHeap(vector<int> &heap)
 
   for (int i = n; i >= 0; i--)
   {
-    heapify(heap, i);
+    maxHeapify(heap, i);
   }
+  printHeap(heap);
 }
 
-void printHeap(vector<int> binaryHeap)
+void heapSort(vector<int> &heap)
 {
-  for (int i = 0; i < binaryHeap.size(); i++)
+  maxHeap(heap);
+  int n = heap.size();
+  for (int i = n - 1; i > 0; i--)
   {
-    cout << binaryHeap[i] << " ";
+    swap(heap[0], heap[i]);
+    heapify(heap, i, 0);
   }
-
-  cout << "\n";
 }
 
 int main()
 {
   vector<int> binaryMeanHeap = {100, 15, 50, 4, 39990, 400, 250, 1000, 20};
+  // vector<int> binaryMeanHeap = {10, 15, 50, 4, 20};
 
   printHeap(binaryMeanHeap);
 
-  maxHeap(binaryMeanHeap);
+  heapSort(binaryMeanHeap);
   printHeap(binaryMeanHeap);
-  cout << "\nLargest: " << binaryMeanHeap[0];
 
   return 0;
 }
